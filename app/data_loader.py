3e504078ce372176c6b24f7b804fa840f3999857
Michail Roesli <miroesli@pop-os.localdomain>
2020-04-29 15:16:19 -0700

    update path settings

--- app/data_loader.py
+++ app/data_loader.py
@@ -10,17 +10,19 @@ import json
 import config
 
 # https://stackoverflow.com/questions/4060221/how-to-reliably-open-a-file-in-the-same-directory-as-a-python-script
-__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+__location__ = os.path.realpath(os.path.join(
+    os.getcwd(), os.path.dirname(__file__)))
 
 from ece_inpaint import inpaint
 
 
 def load_image_names_and_path():
-    return [f for f in glob.glob(__location__ + '\\' + config.input_folder + '\\*.png') if os.path.isfile(os.path.join(__location__, f))]
+    return [f for f in glob.glob(os.path.join(__location__, config.input_folder, '*.png')) if os.path.isfile(os.path.join(__location__, f))]
+# os.path.join(__location__, config.temp_folder, new_file_name + '.png')
 
 
 def load_mask_data():
-    with open(str(os.path.join(__location__, config.input_folder + '\\maskdata.json')), 'r') as file:
+    with open(str(os.path.join(__location__, config.input_folder + '/maskdata.json')), 'r') as file:
         return json.load(file)
 
 
@@ -35,4 +37,5 @@ for image_path in image_paths:
     image_file = os.path.basename(image_path)
     image_name, ext = os.path.splitext(image_file)
     final_image = inpaint(image_file, mask_data[image_file])
-    cv2.imwrite('\\' + config.output_folder + '\\' + image_name + '-inpainted.png', final_image)
+    cv2.imwrite(os.path.join(__location__, config.output_folder,
+                             image_name + '-inpainted.png'), final_image)
