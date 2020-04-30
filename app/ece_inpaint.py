cacc49c3830aa3b53940c5b2f22cfcb8c999ee8b
BuildTools <misha.roesli@gmail.com>
2020-04-30 13:17:08 -0700

    add missing square brackets

--- app/ece_inpaint.py
+++ app/ece_inpaint.py
@@ -81,7 +81,7 @@ def inpaint(image_file, mask):
     gradients = np.stack((fx, fy), axis=-1)
     print(gradients.shape)
 
-    G = np.array([np.sum([np.dot(channel, channel) for channel in x]) for x in y] for y in gradients)
+    G = np.array([[np.sum([np.dot(channel, channel) for channel in x]) for x in y] for y in gradients])
     print(G.shape)
 
     # get the eigenvalues and eigenvectors
